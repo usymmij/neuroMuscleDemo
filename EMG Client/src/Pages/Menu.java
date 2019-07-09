e20323a27209e0fbfb686eb51000e2344e5c2e9a
(∩ ͡° ͜ʖ ͡°)⊃━☆ﾟ. * ･ ｡ﾟ <>
2019-07-09 11:13:05 +0800

    added-interactive-options-menu

--- EMG Client/src/Pages/Menu.java
+++ EMG Client/src/Pages/Menu.java
@@ -10,22 +10,24 @@ public class Menu extends Page {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for(int i = 0; i < 5; i++) {
+        for(int i = 0; i < 6; i++) {
             g.fillRect(getWidth() / 2 - 500,
-                    ((getHeight() / 8) * (i + 1)) + (i * 50),
+                    ((getHeight() / 10) * (i + 1)) + (i * 10),
                     1000, 100);
         }
         //remember to add auto text removal for the current mode in the future
         g.setColor(Color.BLACK);
         g.drawString("Uninterpolated data", getWidth() / 2 - 480,
-                (getHeight() / 8) + 70);
+                (getHeight() / 10) + 70);
         g.drawString("derivative data", getWidth() / 2 - 480,
-                (getHeight() / 8 * 2) + 120);
+                (getHeight() / 10 * 2) + 90);
         g.drawString("add servo to pin 12", getWidth() / 2 - 480,
-                (getHeight() / 8 * 3) + 175);
+                (getHeight() / 10 * 3) + 100);
         g.drawString("switch input method (audio/serial)", getWidth() / 2 - 480,
-                (getHeight() / 8 * 4) + 225);
+                (getHeight() / 10 * 4) + 110);
         g.drawString("change colour scheme", getWidth() / 2 - 480,
-                (getHeight() / 8 * 5) + 275);
+                (getHeight() / 10 * 5) + 120);
+        g.drawString("Reconnect arduino", getWidth() / 2 - 480,
+                (getHeight() / 10 * 6) + 130);
     }
 }
